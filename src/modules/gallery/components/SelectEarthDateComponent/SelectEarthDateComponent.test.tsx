2b666680f3c66757511c500d2b9856917ba7f167
Omar Guanoluisa <omar.guanoluisa25@gmail.com>
2024-09-02 22:34:07 -0500

    functionality to close and change label in button was added

--- src/modules/gallery/components/SelectEarthDateComponent/SelectEarthDateComponent.test.tsx
+++ src/modules/gallery/components/SelectEarthDateComponent/SelectEarthDateComponent.test.tsx
@@ -9,12 +9,31 @@ describe('SelectEarthDateComponent', () => {
     render(<SelectEarthDateComponent />)
 
     // Act
-    const button = screen.getByRole('button', { name: /earth date/i })
+    const button = screen.getByRole('button')
 
     fireEvent.click(button)
     const modal = screen.getByRole('dialog')
 
     // Assert
     expect(modal).toBeInTheDocument()
+  })
+
+  it('should change label of button when user choose a date and modal should close', () => {
+    // Arrange
+    render(<SelectEarthDateComponent />)
+
+    // Act
+    const button = screen.getByRole('button')
+
+    fireEvent.click(button)
+
+    const date = screen.queryAllByRole('gridcell')
+    const modal = screen.getByRole('dialog')
+
+    fireEvent.click(date[0])
+
+    // Assert
+    expect(button).not.toHaveTextContent(/earth date/i)
+    expect(modal).not.toBeInTheDocument()
   })
 })
--- src/modules/gallery/components/SelectEarthDateComponent/SelectEarthDateComponent.tsx
+++ src/modules/gallery/components/SelectEarthDateComponent/SelectEarthDateComponent.tsx
@@ -1,6 +1,7 @@
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import dayjs, { Dayjs } from 'dayjs'
 import { useState } from 'react'
 
 import ModalComponent from '../ModalComponent/ModalComponent'
@@ -8,18 +9,27 @@ import SelectButtonComponent from '../SelectButtonComponent/SelectButtonComponen
 
 const SelectEarthDateComponent: React.FC = () => {
   const [open, setOpen] = useState(false)
+  const [earthDate, setEarthDate] = useState<Dayjs | null>(dayjs(new Date()))
 
   const onChangeOpen = (): void => {
     setOpen(!open)
   }
 
+  const onChangeDate = (date: Dayjs): void => {
+    setEarthDate(date)
+    onChangeOpen()
+  }
+
   return (
     <>
-      <SelectButtonComponent title="Earth Date" onShow={onChangeOpen} />
+      <SelectButtonComponent
+        title={earthDate ? dayjs(earthDate).format('DD-MM-YYYY') : 'Earth Date'}
+        onShow={onChangeOpen}
+      />
       {open && (
-        <ModalComponent open={open} onClose={onChangeOpen} title="Cameras">
+        <ModalComponent open={open} onClose={onChangeOpen} title="Earth Date">
           <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateCalendar />
+            <DateCalendar value={earthDate} onChange={onChangeDate} />
           </LocalizationProvider>
         </ModalComponent>
       )}
