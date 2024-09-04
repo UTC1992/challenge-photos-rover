19c8179d73263a511a8bb659b839085f64963715
Omar Guanoluisa <omar.guanoluisa25@gmail.com>
2024-09-04 01:26:13 -0500

    scroll horizontal was integrated

--- src/modules/gallery/components/BookMarkListComponent/BookMarkListComponent.tsx
+++ src/modules/gallery/components/BookMarkListComponent/BookMarkListComponent.tsx
@@ -1,9 +1,13 @@
-import { Box, Grid2, Stack, Tooltip } from '@mui/material'
+import { Box, Grid2, Stack, Tab, Tabs, Tooltip } from '@mui/material'
 
 import { useListenBookmarks } from '../../hooks/useListenBookmarks'
 import BookmarkComponent from '../BookmarkComponent/BookmarkComponent'
 import { useDeleteBookmark } from '../../hooks/useDeleteBookmark'
-import { BookmarkContainerStyled } from './styledComponents'
+import {
+  BookmarkContainerStyled,
+  TabsStyled,
+  TabStyled,
+} from './styledComponents'
 import { useControlFilter } from '../../hooks/useControlFilter'
 
 const BookMarkListComponent: React.FC = () => {
@@ -15,26 +19,27 @@ const BookMarkListComponent: React.FC = () => {
     <Grid2 container columns={24}>
       <Grid2 size={{ xs: 1, sm: 1, md: 2, lg: 3, xl: 6 }} />
       <Grid2 size={{ xs: 22, sm: 22, md: 20, lg: 18, xl: 12 }}>
-        <Stack
-          direction="row"
-          columnGap={1}
-          sx={{
-            overflowX: 'scroll',
-            scrollbarWidth: 'none',
-          }}
+        <TabsStyled
+          value={0}
+          variant="scrollable"
+          scrollButtons
+          allowScrollButtonsMobile
+          TabIndicatorProps={{ style: { display: 'none' } }}
         >
           {items.map(({ id, ...rest }, index) => (
-            <BookmarkContainerStyled
+            <TabStyled
               key={'bookmark-' + id}
-              onClick={() => onSetFilter(rest)}
-            >
-              <BookmarkComponent
-                title={`${index + 1} - ${rest.rover} - ${rest.camera}`}
-                onDelete={() => onDeleteBookmark(id)}
-              />
-            </BookmarkContainerStyled>
+              label={
+                <BookmarkContainerStyled onClick={() => onSetFilter(rest)}>
+                  <BookmarkComponent
+                    title={`${index + 1} - ${rest.rover} - ${rest.camera}`}
+                    onDelete={() => onDeleteBookmark(id)}
+                  />
+                </BookmarkContainerStyled>
+              }
+            />
           ))}
-        </Stack>
+        </TabsStyled>
       </Grid2>
       <Grid2 size={{ xs: 1, sm: 1, md: 2, lg: 3, xl: 6 }} />
     </Grid2>
